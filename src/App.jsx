fe05ac7d6d1b8ceb6426411d7ad456a2266e8e31
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-02-22 03:56:22 -0300

    chore: add tailwind prettier plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   const name = "name"
   console.log(name)
-  return <h1 className="font-bold text-1xl">Hello worlddd</h1>
+  return <h1 className="text-1xl font-bold">Hello worlddd</h1>
 }
 
 export default App
