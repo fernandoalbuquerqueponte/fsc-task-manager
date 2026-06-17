c41a41e855f55ed0a7e7ef1296894e14678ad6c6
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-17 16:08:36 -0300

    feat: add InputErrorMessage component

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,5 +1,6 @@
 import { forwardRef } from "react"
 
+import InputErrorMessage from "./InputErrorMessage"
 import InputLabel from "./InputLabel"
 
 const TimeSelect = forwardRef((props, ref) => {
@@ -17,9 +18,7 @@ const TimeSelect = forwardRef((props, ref) => {
         <option value="evening">Noite</option>
       </select>
       {props.errorMessage && (
-        <span className="text-left text-xs text-red-500">
-          {props.errorMessage}
-        </span>
+        <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
       )}
     </div>
   )
