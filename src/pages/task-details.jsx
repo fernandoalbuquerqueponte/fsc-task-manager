5e4b584ff35d7ff477852e4791c7341279673c3a
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-21 11:31:33 -0300

    feat: ensure task can be deleted

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -83,6 +83,19 @@ const TaskDetailsPage = () => {
     toast.success("Tarefa salva com sucesso!")
   }
 
+  const handleDeleteClick = async () => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    })
+
+    if (!response.ok) {
+      return toast.error("Ocorreu um erro ao deletar tarefa.")
+    }
+
+    navigate(-1)
+    toast.success("Tarefa deletada com sucesso!")
+  }
+
   const titleError = errors.find((error) => error.inputName === "title")
   const timeError = errors.find((error) => error.inputName === "time")
   const descriptionError = errors.find(
@@ -129,7 +142,11 @@ const TaskDetailsPage = () => {
           </div>
 
           {/* parte da direita */}
-          <Button className="h-fit self-end" color="danger">
+          <Button
+            className="h-fit self-end"
+            color="danger"
+            onClick={handleDeleteClick}
+          >
             <TrashIcon />
             Deletar tarefa
           </Button>
