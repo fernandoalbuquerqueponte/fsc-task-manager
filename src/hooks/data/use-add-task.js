ecc416e52acb7f1466f1c9ea5fd611f7e251707c
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-22 09:01:58 -0300

    refactor: add axios

--- src/hooks/data/use-add-task.js
+++ src/hooks/data/use-add-task.js
@@ -1,19 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-
+import axios from "axios"
 export const useAddTask = () => {
   const queryClient = useQueryClient()
   return useMutation({
     mutationKey: ["addTask"],
     mutationFn: async (task) => {
-      const response = await fetch("http://localhost:3000/tasks", {
-        method: "POST",
-        body: JSON.stringify(task),
-      })
-      if (!response.ok) {
-        throw new Error()
-      }
+      const { data: createdTask } = await axios.post(
+        "http://localhost:3000/tasks",
+        task
+      )
 
-      const createdTask = await response.json()
       return createdTask
     },
     onSuccess: (createdTask) => {
